c319609143cb23057de8f2a797009524505c7339
Shafique Shaikh <your_email@example.com>
2024-01-18 12:04:14 +0530

    Mode btn

--- src/Components/Darkmodetogle.js
+++ src/Components/Darkmodetogle.js
@@ -2,18 +2,17 @@ import React from "react";
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 import weatherapp from '../asset/weatherapp.png'
 
-const DarkModeToggle = ({ toggleTheme,  setTheme }) => {
+const DarkModeToggle = ({ toggleTheme, setTheme }) => {
   return (
-    <div className="flex justify-between">
-      
-      <div className="flex p-4 my-1 mt-2 cursor-pointer md-5 w-fit">
+    <div className="flex flex-col justify-between md:flex-row">
+      <div className="flex p-4 my-1 mt-2 cursor-pointer md:w-fit">
         <img src={weatherapp} alt="weather-app" className="w-20 h-20" />
         <h2 className='m-5 text-5xl font-bold dark:text-white '>Weather App</h2>
       </div>
 
-      <div className="p-4 my-1 mt-2 cursor-pointer md-5 w-fit">
-        <div className="container">
-          <div className="switch">
+      <div className="p-4 my-1 mt-2 cursor-pointer md:w-fit">
+        <div className="container flex">
+          <div className="switch md:justify-end">
             <label htmlFor="toggle">
               <input
                 id="toggle"
